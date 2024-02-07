974689cdee01bb2f6fba4ad5f5522b48a028e8a1
Ch3did <phchedidk@gmail.com>
2024-02-07 11:55:10 -0300

    feat: added module files

---
+++ src/env.py
@@ -0,0 +1,11 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv(".env")
+
+API_TOKEN = os.environ.get("api_token")
+EMAIL = os.environ.get("email")
+DOMAIN = os.environ.get("domain")
+DATABASE_URL=os.environ.get("database_url")
+DEBUG=os.environ.get("debug") 
