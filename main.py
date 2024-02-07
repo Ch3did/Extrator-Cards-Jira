7046885f6e9584ee15f1c005f78eda83f85ddcc8
Ch3did <phchedidk@gmail.com>
2024-02-07 11:55:19 -0300

    feat: created main

---
+++ main.py
@@ -0,0 +1,9 @@
+from src.get_env import API_TOKEN, DOMAIN, EMAIL
+from src.views.jira import JiraView
+
+if __name__ == "__main__":
+    jira = JiraView(domain=DOMAIN, api_token=API_TOKEN, email=EMAIL)
+    try:
+        jira.process()
+    except Exception as error:
+        print(error)
