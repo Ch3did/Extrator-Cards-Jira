78200eb61f719082e39ea602c39688c85057a0dc
Ch3did <phchedidk@gmail.com>
2024-02-14 17:40:16 -0300

    feat: added pagination

--- src/controller/jira_api.py
+++ src/controller/jira_api.py
@@ -17,24 +17,29 @@ class JiraAPI:
         self.board = BoardController()
         self.issue = IssueController()
 
-    def _get_token(self):
+    def _get_token(self) -> str:
         string = f"{self.email}:{self.api_token}"
         sample_string_bytes = string.encode("ascii")
         base64_bytes = base64.b64encode(sample_string_bytes)
         base64_string = base64_bytes.decode("ascii")
         return f"Basic {base64_string}"
 
-    def _make_headers(self):
-        return {"Authorization": f"{self._get_token()}"}
+    def _make_headers(self, page: int) -> dict:
+        results = 100
+        return {
+            "Authorization": f"{self._get_token()}",
+            "startAt" : f"{page*results}",
+            "maxResults": f"{results}"
+        }
 
-    def _get_boards_info(self) -> dict:
+    def _get_boards_info(self, page: int = 0) -> dict:
         url = f"{self.domain}rest/agile/1.0/board/"
-        headers = self._make_headers()
+        headers = self._make_headers(page)
         response = requests.get(url, headers=headers)
         response.raise_for_status()
         return response.json()
 
-    def _get_issues_info(self, board_id: int) -> dict:
+    def _get_issues_info(self, board_id: int, page: int = 0) -> dict:
         url = f"{self.domain}/rest/agile/1.0/board/{board_id}/issue"
         headers = self._make_headers()
         response = requests.get(url, headers=headers)
