1775990dbb25adbcc062fe27d511fba4db492746
Ch3did <phchedidk@gmail.com>
2024-02-14 17:40:01 -0300

    feat: added pagination | solve get_issues_id reffs

--- src/views/jira.py
+++ src/views/jira.py
@@ -6,17 +6,25 @@ from src.controller.jira_api import JiraAPI
 class JiraView(JiraAPI):
     # TODO: adicionar paginação
     def get_boards_id(self) -> List:
-        jira_boards = self._get_boards_info()
-        boards = self.board.board_factory(jira_boards)
+        boards = []
+        is_last_page = False
+        while not is_last_page:
+            jira_boards = self._get_boards_info()
+            boards.append(self.board.board_factory(jira_boards))
+            is_last_page = jira_boards['isLast']
         return boards
 
     def get_issues_id(self, board: int)-> List:
-        jira_issues = self._get_issues_info(board)
-        issues = self.issue.issues_factory(jira_issues["issues"])
+        issues = []
+        is_last_page = False
+        while not is_last_page:
+            jira_issues = self._get_issues_info(board)
+            issues.appned(self.issue.issues_factory(jira_issues["issues"]))
+            is_last_page = jira_issues["isLast"]
         return issues
 
     def process(self):
         boards_id_list = self.get_boards_id()
         for board in boards_id_list:
-            issues_id_list = self.get_issues(board)
+            issues_id_list = self.get_issues_id(board)
         return True if issues_id_list else False
