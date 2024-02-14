98818c8f0526641d6789666458377031ea034b95
Ch3did <phchedidk@gmail.com>
2024-02-14 17:24:14 -0300

    feat: solve database

--- database.py
+++ database.py
@@ -1,9 +1,11 @@
-from sqlalchemy import create_engine
+from sqlmodel import create_engine
 
 from src.env import DATABASE_URL, DEBUG
-from src.models import _run_boar_model, _run_issue_model
+from src.models.board import _run_board_model
+from src.models.issue import _run_issue_model
+
+engine = create_engine(DATABASE_URL, echo=bool(DEBUG))
 
 if __name__ == "__main__":
-    engine = create_engine(DATABASE_URL)
-    _run_boar_model(engine)
+    _run_board_model(engine)
     _run_issue_model(engine)
