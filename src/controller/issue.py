81ab15bbdf5b95a2d7ae617e48f51f3bb1717427
Ch3did <phchedidk@gmail.com>
2024-02-15 17:46:58 -0300

    fix: solve IssueController reffs

--- src/controller/issue.py
+++ src/controller/issue.py
@@ -11,12 +11,22 @@ class IssueController(DatabaseController):
         timespent_value = fields_dict.get("timespent")
         resolution_date_value = fields_dict.get("resolutiondate")
         status_category_change_date_value = fields_dict.get("statuscategorychangedate")
-
+        sprint = fields_dict["sprint"].get("id") if fields_dict.get("sprint") else None
+        resolution = (
+            fields_dict["resolution"].get("id")
+            if fields_dict.get("resolution")
+            else None
+        )
+        issue_type_id = (
+            fields_dict["issuetypeid"].get("id")
+            if fields_dict.get("issuetypeid")
+            else None
+        )
         field = Fields(
-            sprint=fields_dict.get("sprint")["id"],
-            resolution=fields_dict.get("resolution")["id"],
+            sprint=sprint,
+            resolution=resolution,
             work_ratio=fields_dict.get("workratio"),
-            issue_type_id=fields_dict.get("issuetype")["id"],
+            issue_type_id=issue_type_id,
             status_category_change_date=(
                 datetime.strptime(
                     status_category_change_date_value[:-9], "%Y-%m-%dT%H:%M:%S"
@@ -49,7 +59,7 @@ class IssueController(DatabaseController):
                 id=issue.get("id"),
                 self_=issue.get("self"),
                 key=issue.get("key"),
-                fields=fields.id,
+                fields_id=fields.id,
             )
             self._add_to_database(issue)
             issue_id_list.append(issue)
