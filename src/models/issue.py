b90f48dd7bdce642c04fbe815e4419954607ee00
Ch3did <phchedidk@gmail.com>
2024-02-15 17:47:32 -0300

    feat: added Optional for some Fields attrs

--- src/models/issue.py
+++ src/models/issue.py
@@ -6,10 +6,10 @@ from sqlmodel import Field, SQLModel
 
 class Fields(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
-    resolution: str
+    resolution: Optional[str]
     work_ratio: int
     # customfield_10032: List[str] #TODO: Validar com o gui
-    issue_type_id: int
+    issue_type_id: Optional[int]
     status_category_change_date: Optional[datetime]
     timespent: Optional[datetime]
     resolution_date: Optional[datetime]
