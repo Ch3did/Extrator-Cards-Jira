93ea0effc05a2885ee49741e5ad1c61572a4d8a8
Ch3did <phchedidk@gmail.com>
2024-02-15 13:20:54 -0300

    feat: added traceback error and log from exception

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from src.env import API_TOKEN, DOMAIN, EMAIL
 from src.views.jira import JiraView
+from loguru import logger
+import traceback
 
 if __name__ == "__main__":
     jira = JiraView(domain=DOMAIN, api_token=API_TOKEN, email=EMAIL)
     try:
         jira.process()
     except Exception as error:
-        print(error)
+        logger.error(error)
+        msg = traceback.format_exc()
+        logger.error(msg)
