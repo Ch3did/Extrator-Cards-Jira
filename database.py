8e3244953bfc6b4ccaa48d131b1e385d35e2d810
Ch3did <phchedidk@gmail.com>
2024-03-26 17:07:18 -0300

    feat: added sprint to database

--- database.py
+++ database.py
@@ -3,9 +3,11 @@ from sqlmodel import create_engine
 from src.env import DATABASE_URL, DEBUG
 from src.models.board import _run_board_model
 from src.models.issue import _run_issue_model
+from src.models.sprint import _run_sprint_model
 
 engine = create_engine(DATABASE_URL, echo=bool(DEBUG))
 
 if __name__ == "__main__":
     _run_board_model(engine)
     _run_issue_model(engine)
+    _run_sprint_model(engine)
