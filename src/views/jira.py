e4112e5e9816f550411841c38d137c758d587c78
Ch3did <phchedidk@gmail.com>
2024-03-26 17:07:36 -0300

    feat: solve JiraView

--- src/views/jira.py
+++ src/views/jira.py
@@ -1,33 +1,56 @@
 from typing import List
 
+from loguru import logger
+
 from src.controller.jira_api import JiraAPI
 
 
 class JiraView(JiraAPI):
     def get_boards(self) -> List:
+        logger.info("Getting boards...")
         boards = []
         is_last_page = False
+        page = 0
         while not is_last_page:
-            jira_boards = self._get_boards_info()
+            jira_boards = self._get_boards_info(page)
             boards = boards + self.board.board_factory(jira_boards)
             is_last_page = jira_boards["isLast"]
+            page += 1
+        logger.info(f"Found {len(boards)} boards...")
         return boards
 
     def get_issues(self, board: int) -> None:
-        issues = []
+        logger.info("Getting Issues...")
         is_last_page = False
+        page = 0
         while not is_last_page:
-            jira_issues = self._get_issues_info(board)
-            self.issue.issues_factory(jira_issues)
+            jira_issues = self._get_issues_info(board, page)
+            self.issue.issues_factory(jira_issues, board)
             is_last_page = (
                 True
-                if jira_issues.get("isLast")
-                or jira_issues["maxResults"] > jira_issues["total"]
+                if jira_issues.get("startAt") + jira_issues.get("maxResults") >= jira_issues.get("total")
                 else False
             )
+            page += 1
 
-    def process(self):
+    def get_sprints(self, board: int) -> None:
+        logger.info("Getting Sprints...")
+        is_last_page = False
+        page = 0
+        while not is_last_page:
+            jira_sprints = self._get_sprints_info(board, page)
+            self.sprint.sprint_factory(jira_sprints, board)
+            is_last_page = (
+                True
+                if jira_sprints.get("isLast")
+                or jira_sprints["maxResults"] > jira_sprints["total"]
+                else False
+            )
+            page += 1
+
+    def process(self, board_name_list) -> None:
         boards_id_list = self.get_boards()
         for board in boards_id_list:
-            issues_id_list = self.get_issues(board)
-        return True if issues_id_list else False
+            logger.info(f"Running JiraAPI for board {board}")
+            self.get_sprints(board)
+            self.get_issues(board)
