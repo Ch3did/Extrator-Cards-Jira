a32f55f719a712a107e764ef16daac0195566a1d
Ch3did <phchedidk@gmail.com>
2024-03-27 01:08:05 -0300

    fix: run pre-commit

--- src/controller/jira_api.py
+++ src/controller/jira_api.py
@@ -25,7 +25,7 @@ class JiraAPI:
 
         Returns:
             str: Authentication token string
-        """        
+        """
         string = f"{self.email}:{self.api_token}"
         sample_string_bytes = string.encode("ascii")
         base64_bytes = base64.b64encode(sample_string_bytes)
