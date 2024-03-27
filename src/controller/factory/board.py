90d01d67f7cc6a5e95b9c33da8c115bcd5883d2e
Ch3did <phchedidk@gmail.com>
2024-03-27 01:26:35 -0300

    fix: removed unused location object

--- src/controller/factory/board.py
+++ src/controller/factory/board.py
@@ -1,33 +1,19 @@
 from src.controller.database import DatabaseController
-from src.models.board import Board, Location
+from src.models.board import Board
 
 
 class BoardController(DatabaseController):
     """Classe responsável pela criação e registro dos Boards"""
 
-    def _location_factory(self, location_data: dict) -> Location:
-        location = Location(
-            location_id=location_data.get("projectId"),
-            location_name=location_data.get("projectName"),
-            display_name=location_data.get("displayName"),
-            location_key=location_data.get("projectKey"),
-            location_type_key=location_data.get("projectTypeKey"),
-            avatar_URI=location_data.get("avatarURI"),
-        )
-        self._add_to_database(location)
-        return location
-
     def board_factory(self, api_response: dict):
         boad_id_list = []
         for board_dict in api_response["values"]:
             if board_dict["type"] == "scrum":
-                location = self._location_factory(board_dict.get("location"))
                 board = Board(
                     board_id=board_dict.get("id"),
                     board_name=board_dict.get("name"),
                     board_url=board_dict.get("self"),
                     board_type=board_dict.get("type"),
-                    location_id=location.id,
                 )
                 boad_id_list.append(board.board_id)
                 self._add_to_database(board)
--- src/models/board.py
+++ src/models/board.py
@@ -4,24 +4,6 @@ from typing import Optional
 from sqlmodel import TIMESTAMP, Column, Field, SQLModel, text
 
 
-class Location(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    location_id: int
-    location_name: str
-    display_name: str
-    location_name: str
-    location_key: str
-    location_type_key: str
-    avatar_URI: str
-    colected_time_stemp: Optional[datetime] = Field(
-        sa_column=Column(
-            TIMESTAMP(timezone=True),
-            nullable=False,
-            server_default=text("CURRENT_TIMESTAMP"),
-        )
-    )
-
-
 class Board(SQLModel, table=True):
     id: Optional[int] = Field(primary_key=True)
     board_id: int
@@ -35,8 +17,6 @@ class Board(SQLModel, table=True):
             server_default=text("CURRENT_TIMESTAMP"),
         )
     )
-
-    location_id: Optional[int] = Field(foreign_key="location.id")
 
 
 def _run_board_model(engine):
