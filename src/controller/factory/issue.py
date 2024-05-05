09d9ea3195f4cf7b0e538085a34c35b40952c4fc
Ch3did <phchedidk@gmail.com>
2024-05-05 20:06:38 -0300

    solve issue factory

--- src/controller/factory/issue.py
+++ src/controller/factory/issue.py
@@ -7,44 +7,53 @@ from src.models.issue import Issue
 class IssueController(DatabaseController):
     """Classe responsável pela criação e registro das Issues"""
 
+    @staticmethod
+    def _solve_time(fields_dict, reffs):
+        time_value = (
+            datetime.strptime(fields_dict.get(reffs)[:-9], "%Y-%m-%dT%H:%M:%S")
+            if fields_dict.get(reffs)
+            else None
+        )
+        return time_value
+
+    @staticmethod
+    def _solve_sprint(sprints_list):
+        sprints = []
+        if isinstance(sprints_list, list):
+            for sprint in sprints_list:
+                sprints.append(sprint["id"])
+        else:
+            if current_sprint_id := sprints_list.get("id"):
+                sprints.append(current_sprint_id)
+
+    @staticmethod
+    def _solve_belonged_sprint(belonged_sprint_list, sprints=[]):
+        sprints = sprints if sprints else []
+        if belonged_sprint_list:
+            for item in belonged_sprint_list:
+                sprints.append(item["id"])
+        return sprints
+
     def issues_factory(self, issues_dict, board) -> Issue:
         for issue in issues_dict["issues"]:
             fields_dict = issue["fields"]
 
-            timespent_value = (
-                datetime.strptime(
-                    fields_dict.get("timespent")[:-9], "%Y-%m-%dT%H:%M:%S"
-                )
-                if fields_dict.get("timespent")
-                else None
-            )
-            creation_date_value = (
-                datetime.strptime(fields_dict.get("created")[:-9], "%Y-%m-%dT%H:%M:%S")
-                if fields_dict.get("created")
-                else None
-            )
-            resolution_date_value = (
-                datetime.strptime(
-                    fields_dict.get("resolutiondate")[:-9], "%Y-%m-%dT%H:%M:%S"
-                )
-                if fields_dict.get("resolutiondate")
-                else None
-            )
-            status_category_change_date_value = (
-                datetime.strptime(
-                    fields_dict.get("statuscategorychangedate")[:-9],
-                    "%Y-%m-%dT%H:%M:%S",
-                )
-                if fields_dict.get("statuscategorychangedate")
-                else None
+            # Data reesolution
+            timespent_value = self._solve_time(fields_dict, "timespent")
+            creation_date_value = self._solve_time(fields_dict, "created")
+            resolution_date_value = self._solve_time(fields_dict, "resolutiondate")
+            status_category_change_date_value = self._solve_time(
+                fields_dict, "statuscategorychangedate"
             )
 
-            closed_sprint = None
-            if sprints := fields_dict.get("closedSprints"):
-                closed_sprint = f"{[item['id'] for item in sprints]}"
+            # Sprint Resolution
+            sprints_list = fields_dict["sprint"] if fields_dict.get("sprint") else {}
+            sprints = self._solve_sprint(sprints_list)
+            belonged_sprint = self._solve_belonged_sprint(
+                fields_dict.get("closedSprints"), sprints
+            )
 
             epic = fields_dict["epic"] if fields_dict.get("epic") else {}
-            sprint = fields_dict["sprint"] if fields_dict.get("sprint") else {}
             creator = fields_dict["creator"] if fields_dict.get("creator") else {}
             priority = fields_dict["priority"] if fields_dict.get("priority") else {}
             reporter = fields_dict["reporter"] if fields_dict.get("reporter") else {}
@@ -66,7 +75,8 @@ class IssueController(DatabaseController):
                 epic_name=epic.get("name"),
                 epic_summary=epic.get("summary"),
                 priority_name=priority.get("name"),
-                sprint=sprint.get("id"),
+                current_sprints=f"{sprints}",
+                belonged_sprint=f"{belonged_sprint}",
                 work_ratio=fields_dict.get("workratio"),
                 reporter_name=reporter.get("displayName"),
                 reportar_mail=reporter.get("emailAddress"),
@@ -77,7 +87,6 @@ class IssueController(DatabaseController):
                 timespent=timespent_value,
                 resolution_date=resolution_date_value,
                 creation_date=creation_date_value,
-                closed_sprint=closed_sprint,
             )
 
             self.save_issue(issue)
