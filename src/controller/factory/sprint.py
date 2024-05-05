3e6cd1c742cfcb8589d645e9f78f8eb48ce82dc8
Ch3did <phchedidk@gmail.com>
2024-05-05 20:18:49 -0300

    fix: create solve time to handle sprint's date

--- src/controller/factory/sprint.py
+++ src/controller/factory/sprint.py
@@ -9,40 +9,30 @@ from src.models.sprint import Sprint
 class SprintController(DatabaseController):
     """Classe responsável pela criação e registro das sprint"""
 
-    def sprint_factory(self, sprint_dict, board) -> Sprint:
+    @staticmethod
+    def _solve_time(sprint, reffs):
         fuso_horario_brasilia = pytz.timezone("America/Sao_Paulo")
+        if date_value := sprint.get(reffs):
+            resolution_date = datetime.strptime(
+                date_value[:-5], "%Y-%m-%dT%H:%M:%S"
+            ).replace(tzinfo=pytz.UTC)
+            return resolution_date.astimezone(fuso_horario_brasilia)
+
+    def sprint_factory(self, sprint_dict, board) -> Sprint:
         dates = {}
         for sprint in sprint_dict["values"]:
 
             # Resolution date
-            if resolution_date_value := sprint.get("completeDate"):
-                resolution_date = datetime.strptime(
-                    resolution_date_value[:-5], "%Y-%m-%dT%H:%M:%S"
-                ).replace(tzinfo=pytz.UTC)
-                dates["resolution_date"] = resolution_date.astimezone(
-                    fuso_horario_brasilia
-                )
+            dates["resolution_date"] = self._solve_time(sprint, "completeDate")
 
             # start date
-            if start_date_value := sprint.get("startDate"):
-                start_date = datetime.strptime(
-                    start_date_value[:-5], "%Y-%m-%dT%H:%M:%S"
-                ).replace(tzinfo=pytz.UTC)
-                dates["start_date"] = start_date.astimezone(fuso_horario_brasilia)
+            dates["start_date"] = self._solve_time(sprint, "startDate")
 
             # end date
-            if end_date_value := sprint.get("endDate"):
-                end_date = datetime.strptime(
-                    end_date_value[:-5], "%Y-%m-%dT%H:%M:%S"
-                ).replace(tzinfo=pytz.UTC)
-                dates["end_date"] = end_date.astimezone(fuso_horario_brasilia)
+            dates["end_date"] = self._solve_time(sprint, "endDate")
 
             # created date
-            if created_date_value := sprint.get("createdDate"):
-                created_date = datetime.strptime(
-                    created_date_value[:-5], "%Y-%m-%dT%H:%M:%S"
-                ).replace(tzinfo=pytz.UTC)
-                dates["created_date"] = created_date.astimezone(fuso_horario_brasilia)
+            dates["created_date"] = self._solve_time(sprint, "createdDate")
 
             sprint = Sprint(
                 sprint_id=sprint.get("id"),
@@ -50,10 +40,10 @@ class SprintController(DatabaseController):
                 self_url=sprint.get("self"),
                 sprint_name=sprint.get("name"),
                 origin_board=sprint.get("originBoardId"),
-                start_date=dates.get("start_date"),
                 resolution_date=dates.get("resolution_date"),
-                created_date=dates.get("created_date"),
+                start_date=dates.get("start_date"),
                 end_date=dates.get("end_date"),
+                created_date=dates.get("created_date"),
             )
 
             self.save_sprint(sprint)
