e22d47506d5bb0c1d49ec2bb05f5b565589bd8bd
Ch3did <phchedidk@gmail.com>
2024-05-08 20:43:45 -0300

    feat: added plot to start

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import traceback
 
 from loguru import logger
 
+from src.controller.plot.evolution_leadtime import EvolutionLeadtime
+from src.controller.plot.leadtime import LeadTimePoTipo
 from src.env import API_TOKEN, DOMAIN, EMAIL
 from src.views.jira import JiraView
 
@@ -16,3 +18,8 @@ if __name__ == "__main__":
         jira._status = "Fail"
     finally:
         logger.info(f"Process has finished! Status: {jira._status}")
+    
+    evol = EvolutionLeadtime().process()
+    lead = LeadTimePoTipo().process()
+    lead.make_plot()
+    evol.make_plot()
