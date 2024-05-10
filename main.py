e4e3a44510d9aab9861fdaafff1fc6e7bfcea723
Ch3did <phchedidk@gmail.com>
2024-05-10 19:57:09 -0300

    fix: set lambda patern

--- main.py
+++ main.py
@@ -6,17 +6,18 @@ from src.env import (ACCESS_KEY_ID, API_TOKEN, BUCKET, DOMAIN, EMAIL,
                      SECRET_ACCESS_KEY)
 from src.views.build_wheel import BuildView
 from src.views.extract import ExtractView
+from pprint import pprint
 
-if __name__ == "__main__":
-    exctract = ExtractView(domain=DOMAIN, api_token=API_TOKEN, email=EMAIL)
-    build = BuildView(
-        access_key_id=ACCESS_KEY_ID, secret_access_key=SECRET_ACCESS_KEY, bucket=BUCKET
-    )
+
+def lambda_handler(event, context):
+    pprint(event)
+    pprint(context)
     try:
+        exctract = ExtractView(domain=DOMAIN, api_token=API_TOKEN, email=EMAIL)
         exctract.process()
-        build.process()
-        build.plot_leadtime_graff()
-        build.plot_evolution_graff()
+        build = BuildView(
+            access_key_id=ACCESS_KEY_ID, secret_access_key=SECRET_ACCESS_KEY, bucket=BUCKET
+        )
     except Exception as error:
         logger.error(error)
         msg = traceback.format_exc()
@@ -24,3 +25,14 @@ if __name__ == "__main__":
         exctract._status = "Fail"
     finally:
         logger.info(f"Process has finished! Status: {exctract._status}")
+        build.process()
+        build.plot_leadtime_graff()
+        build.plot_evolution_graff()
+        return {
+            'statusCode': 200,
+            'body': 'Done'
+        }
+
+if __name__ == "__main__":
+        lambda_function()
+    
