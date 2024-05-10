19efb4056b599a9f0f143d8249e384655584d8b2
Ch3did <phchedidk@gmail.com>
2024-05-10 20:31:01 -0300

    fix: solve Dababase name

--- database.py
+++ database.py
@@ -1,10 +1,15 @@
+import os
+
 from sqlmodel import create_engine
 
-from src.env import DATABASE_URL, DEBUG
+from src.env import COMPANY_NAME, DATABASE_URL, DEBUG
 from src.models.board import _run_board_model
 from src.models.changelog import _run_changelog_model
 from src.models.issue import _run_issue_model
 from src.models.sprint import _run_sprint_model
+
+if os.path.exists("/tmp/test_app.db"):
+    os.rename("/tmp/test_app.db", f"./tmp/{COMPANY_NAME}.db")
 
 engine = create_engine(DATABASE_URL, echo=bool(DEBUG))
 
--- src/env.py
+++ src/env.py
@@ -8,7 +8,8 @@ API_TOKEN = os.environ.get("api_token")
 EMAIL = os.environ.get("email")
 DOMAIN = os.environ.get("domain")
 
-DATABASE_URL = f"sqlite:///tmp/{DOMAIN[8:-10]}.db"
+COMPANY_NAME = DOMAIN[8:-10].replace(".atlassian", "")
+DATABASE_URL = f"sqlite:///tmp/{COMPANY_NAME}.db"
 DEBUG = os.environ.get("debug")
 
 # AWS variables
