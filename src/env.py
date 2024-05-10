901e182c499e8cff802b0caf8701ccb0c4797424
Ch3did <phchedidk@gmail.com>
2024-05-10 15:44:03 -0300

    fix: solve DATABASE_URL name logic

--- src/env.py
+++ src/env.py
@@ -7,5 +7,11 @@ load_dotenv(".env")
 API_TOKEN = os.environ.get("api_token")
 EMAIL = os.environ.get("email")
 DOMAIN = os.environ.get("domain")
-DATABASE_URL = os.environ.get("database_url")
+
+DATABASE_URL = f"sqlite:///tmp/{DOMAIN[8:-10]}.db"
 DEBUG = os.environ.get("debug")
+
+# AWS variables
+ACCESS_KEY_ID = os.environ.get("access_key_id")
+SECRET_ACCESS_KEY = os.environ.get("secret_access_key")
+BUCKET = os.environ.get("bucket_name")
